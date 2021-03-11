837c26b23f8ea6e925d7fa11c06e37fc1f9a1e48
dimicorn <zagorulia.ds@phystech.edu>
2021-03-11 11:01:10 +0300

    exe 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
-#include "add elem.cpp"
-int main() {
+//#pragma once
+
+using namespace std;
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+void push_left(Node ** head, int value){
+    if(*head == NULL){
+        *head = (Node*)malloc(sizeof(Node));
+        (**head).value = value;
+        (**head).next = NULL;
+    }
+    else{
+        Node* a = (Node*)malloc(sizeof(Node));
+        (*a).next = *head;
+        (*a).value = value;
+        *head = a;
+    }
+}
+
+int main(){
     return 0;
+
 }
